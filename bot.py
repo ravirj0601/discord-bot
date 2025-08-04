5178c5b7f7f3b75ed3ffe76fc63d59c38d1dc40d
ravirj0601 <ravirj0601@gmail.com>
2025-08-04 23:09:50 +0530

    Some bug fixes in a Github fixes.

--- bot.py
+++ bot.py
@@ -50,10 +50,21 @@ async def add(ctx, *args):
 @bot.command()
 async def gitfetch(ctx, git_username, git_repo_name):
     git_info = gitcord.fetch_gitinfo(git_username, git_repo_name)
-    username = git_info["user_name"]
-    repo = git_info["repository"]
-    commit_id = git_info["commit_id"]
-    last_commit_date = git_info["last_commit_date"]
+    if git_info:
+        username = git_info["commit"]["author"]["name"]
+        committer_name = git_info["commit"]["committer"]["name"]
+        commit_id = git_info["sha"][:7]
+        last_commit_date = git_info["commit"]["author"]["date"][:10]
+        response = (
+            f"Author: {username}\n"
+            f"Commiter Name: {committer_name}\n"
+            f"Repository: {git_repo_name}\n"
+            f"Commit ID: {commit_id}\n"
+            f"Last Commit Date: {last_commit_date}"
+        )
+    else:
+        response = "Could not fetch GitHub info."
+    
     await ctx.send(response)
 
 
