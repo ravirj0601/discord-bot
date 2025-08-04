f444a3007bb8aae9eb3dad19fb24427949e5c5ea
Arjun Rajan <arjunrajan991@gmail.com>
2025-08-04 21:22:40 +0530

    the right way to do bot commands

--- bot.py
+++ bot.py
@@ -1,32 +1,38 @@
 import discord
+from discord.ext import commands
 from config import BOT_TOKEN
-import handle_client
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-@client.event
+
+@bot.event
 async def on_ready():
-    print(f"We have logged in as {client.user}")
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content[0] == '!':
-        try:
-            response_pkt = handle_client.get_response(message)
-            await message.channel.send(response_pkt)
-        except ValueError:
-            response_pkt = "No such command exists"
-            print(response_pkt)
-            await message.channel.send(response_pkt)
-        except Exception as e:
-            print(f"[on_message] Error occured: {e}")
+    print(f"We have logged in as {bot.user}")
 
 
-client.run(BOT_TOKEN)
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello, {ctx.author.display_name}!")
+
+
+@bot.command()
+async def hi(ctx):
+    await ctx.send(f"Hi, {ctx.author.display_name}!")
+
+
+@bot.command()
+async def goodbye(ctx):
+    await ctx.send(f"Goodbye, {ctx.author.display_name}!")
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+
+bot.run(BOT_TOKEN)
 
