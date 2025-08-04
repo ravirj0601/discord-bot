742b319a687a1f7c100a42b0a10f57abdd6f0fc7
Arjun Rajan <arjunrajan991@gmail.com>
2025-08-04 22:22:58 +0530

    add and git info command

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
+#!.venv/bin/python3.12
 import discord
 from discord.ext import commands
 from config import BOT_TOKEN
+import json
 
 
 intents = discord.Intents.default()
@@ -32,6 +34,26 @@ async def goodbye(ctx):
 @bot.command()
 async def ping(ctx):
     await ctx.send("Pong!")
+
+
+@bot.command()
+async def add(ctx, *args):
+    try: 
+        nums = [float(x) for x in args]
+        ans = sum(nums)
+        await ctx.send(ans)
+    except:
+        await ctx.send("addition allowed for numbers only")
+
+
+#@bot.command()
+#async def gitfetch(ctx, git_username, git_repo_name):
+    #git_info = gitcord.fetch_gitinfo(git_username, git_repo_name)
+    #username = git_info["user_name"]
+    #repo = git_info["repository"]
+    #commit_id = git_info["commit_id"]
+    #last_commit_date = git_info["last_commit_date"]
+    #await ctx.send(response)
 
 
 bot.run(BOT_TOKEN)
